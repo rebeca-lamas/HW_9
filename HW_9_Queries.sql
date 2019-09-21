11d34804db5c12612a5a8f24931807510059de5f
Hokie <52640930+rebeca-lamas@users.noreply.github.com>
2019-09-21 10:16:54 -0400

    HW 9 solutions

---
+++ HW_9_Queries.sql
@@ -0,0 +1,76 @@
+﻿--1. List following details of eachemployee: employee number, last name, first name, gender, salary
+SELECT Employees.emp_no, last_name, first_name, gender, salary
+FROM Employees
+INNER JOIN Salaries
+ON Employees.emp_no = Salaries.emp_no; 
+
+--2. List employees hired in 1986
+SELECT emp_no, hire_date FROM Employees
+WHERE hire_date 
+LIKE '1986%';
+
+--3. List the manager of each department with the following information: department number, 
+--department name, the manager's employee number, last name, first name, and start and end employment dates.
+SELECT Department_Managers.dept_no, 
+Departments.dept_name, 
+Department_Managers.emp_no,
+last_name,
+first_name,
+from_date,
+to_date
+FROM Department_Managers
+INNER JOIN Departments
+ON Department_Managers.dept_no = Departments.dept_no
+INNER JOIN Employees
+ON Department_Managers.emp_no = Employees.emp_no;
+
+--4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT Department_Employees.emp_no,
+last_name,
+first_name,
+Departments.dept_name
+FROM Department_Employees
+INNER JOIN Departments
+ON Department_Employees.dept_no = Departments.dept_no
+INNER JOIN Employees
+ON Department_Employees.emp_no = Employees.emp_no;
+
+--5.List all employees whose first name is "Hercules" and last names begin with "B."
+SELECT
+first_name,
+last_name
+FROM Employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+--6. List all employees in the Sales department, including their employee number, 
+--last name, first name, and department name.
+SELECT Department_Employees.emp_no,
+last_name,
+first_name,
+Departments.dept_name
+FROM Department_Employees
+INNER JOIN Departments
+ON Department_Employees.dept_no = Departments.dept_no
+INNER JOIN Employees
+ON Department_Employees.emp_no = Employees.emp_no
+WHERE dept_name = 'Sales';
+
+--7.List all employees in the Sales and Development departments, including their employee number,
+--last name, first name, and department name.
+SELECT Department_Employees.emp_no,
+last_name,
+first_name,
+Departments.dept_name
+FROM Department_Employees
+INNER JOIN Departments
+ON Department_Employees.dept_no = Departments.dept_no
+INNER JOIN Employees
+ON Department_Employees.emp_no = Employees.emp_no
+WHERE dept_name = 'Sales' OR dept_name = 'Development';
+
+--8. descending order, list the frequency count of employee last names
+SELECT last_name, COUNT(last_name) AS "name count"
+FROM Employees
+GROUP BY last_name
+ORDER BY "name count" DESC;
---
+++ HW_9_Schema.sql
@@ -0,0 +1,81 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE Departments (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+    CONSTRAINT pk_Departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE Department_Employees (
+    emp_no VARCHAR   NOT NULL,
+    dept_no VARCHAR   NOT NULL,
+    from_date VARCHAR   NOT NULL,
+    to_date VARCHAR   NOT NULL,
+    CONSTRAINT pk_Department_Employees PRIMARY KEY (
+        emp_no, dept_no
+     )
+);
+
+CREATE TABLE Department_Managers (
+    dept_no VARCHAR   NOT NULL,
+    emp_no VARCHAR   NOT NULL,
+    from_date VARCHAR   NOT NULL,
+    to_date VARCHAR   NOT NULL,
+    CONSTRAINT pk_Department_Managers PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE Employees (
+    emp_no VARCHAR   NOT NULL,
+    birth_date VARCHAR   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    gender VARCHAR   NOT NULL,
+    hire_date VARCHAR   NOT NULL,
+    CONSTRAINT pk_Employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE Salaries (
+    emp_no VARCHAR   NOT NULL,
+    salary INTEGER   NOT NULL,
+    from_date VARCHAR   NOT NULL,
+    to_date VARCHAR   NOT NULL,
+    CONSTRAINT pk_Salaries PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE Titles (
+    emp_no VARCHAR   NOT NULL,
+    title VARCHAR   NOT NULL,
+    from_date VARCHAR   NOT NULL,
+    to_date VARCHAR   NOT NULL,
+    CONSTRAINT pk_Titles PRIMARY KEY (
+        emp_no, title
+     )
+);
+
+ALTER TABLE Department_Employees ADD CONSTRAINT fk_Department_Employees_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+ALTER TABLE Department_Employees ADD CONSTRAINT fk_Department_Employees_dept_no FOREIGN KEY(dept_no)
+REFERENCES Departments (dept_no);
+
+ALTER TABLE Department_Managers ADD CONSTRAINT fk_Department_Managers_dept_no FOREIGN KEY(dept_no)
+REFERENCES Departments (dept_no);
+
+ALTER TABLE Department_Managers ADD CONSTRAINT fk_Department_Managers_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+ALTER TABLE Salaries ADD CONSTRAINT fk_Salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+ALTER TABLE Titles ADD CONSTRAINT fk_Titles_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
